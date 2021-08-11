4670eb512c3f2d0726c63548607ea34fdc22dbc8
Vinayak <vseth572@gmail.com>
2021-08-11 23:05:03 +0530

    Final Commit

--- Classification_model.py
+++ Classification_model.py
@@ -3,8 +3,8 @@ import pandas as pd
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn import linear_model, model_selection, preprocessing, naive_bayes, svm
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
@@ -16,6 +16,7 @@ tok = WordPunctTokenizer()
 pat1 = r'@[A-Za-z0-9]+'
 pat2 = r'https?://[A-Za-z0-9./]+'
 combined_pat = r'|'.join((pat1, pat2))
+
 
 PATH_TRAIN = "C:/Users/vs_00/Desktop/Internship/SAS/NLP_Disaster_Tweets/train.csv"
 PATH_TEST = "C:/Users/vs_00/Desktop/Internship/SAS/NLP_Disaster_Tweets/test.csv"
@@ -56,62 +57,82 @@ def normalize(comment, lowercase, remove_stopwords):
     return " ".join(lemmatized)
 
 
-train_x, test_x, train_y, test_y = train_test_split(train_df["text"], train_df["target"], test_size=0.3)
-
 train_df['text'] = train_df['text'].apply(normalize, lowercase=True, remove_stopwords=True)
 test_df['text'] = test_df['text'].apply(normalize, lowercase=True, remove_stopwords=True)
 
-# # Making WorCloud
-# wordcloud = WordCloud(background_color="white", width=3000, height=2000, max_words=300).generate(' '.join(train_df['text'].transpose()))
-# # set the figsize
-# plt.figure(figsize=[15,10])
-# # plot the wordcloud
-# plt.imshow(wordcloud, interpolation="bilinear")
-# # remove plot axes
-# plt.axis("off")
-# plt.show()
+
+train_x, test_x, train_y, test_y = train_test_split(train_df["text"], train_df["target"], test_size=0.3)
+
 
 tfidf_vectorizer = TfidfVectorizer()
 tfidf_vectorizer.fit(train_df['text'])
-train_x_tfidf = tfidf_vectorizer.transform(train_x)
+
+train_x_tfidf = tfidf_vectorizer.fit_transform(train_x)
 test_x_tfidf = tfidf_vectorizer.transform(test_x)
 
-# Ridge Classifier
-clf = linear_model.RidgeClassifier()
-clf.fit(train_x_tfidf, train_y)
-predictions_ridge = clf.predict(test_x_tfidf)
-confusion_matrix_ridge = confusion_matrix(test_y, predictions_ridge)
-print("confusion matrix for Ridge classifier ", confusion_matrix_ridge)
-print("Ridge Classifier Accuracy Score ->", accuracy_score(predictions_ridge, test_y)*100)
-
-# Naive Bayes Classifier
-Naive = naive_bayes.MultinomialNB()
-Naive.fit(train_x_tfidf, train_y)
-
-# predicting the labels on the validation set
-predictions_NB = Naive.predict(test_x_tfidf)
-
-confusion_matrix_nb = confusion_matrix(test_y, predictions_NB)
-print("Confusion Matrix for Naive Bayes ",confusion_matrix_nb)
-
-# Using the accuracy score function to get the accuracy score
-print("Naive Bayes Accuracy Score ->", accuracy_score(predictions_NB, test_y)*100)
 
 # SVM Classifier
-SVM = svm.SVC()
+SVM = svm.SVC(C=1.0, kernel='linear', degree=4, gamma=0.1)
 SVM.fit(train_x_tfidf, train_y)
 predictions_svm = SVM.predict(test_x_tfidf)
-
 confusion_matrix_svm = confusion_matrix(test_y, predictions_svm)
 print("confusion matrix for svm ", confusion_matrix_svm)
-print("SVM Accuracy Score ->", accuracy_score(predictions_svm, test_y)*100)
+print("SVM Accuracy Score ->", accuracy_score(predictions_svm, test_y) * 100)
 
-# Logistic Classifier
-logreg = LogisticRegression()
-logreg.fit(train_x_tfidf, train_y)
-predictions_logistic = logreg.predict(test_x_tfidf)
-confusion_matrix_log = confusion_matrix(test_y, predictions_logistic)
-print("Confusion Matrix for logistic", confusion_matrix_log)
-print("Logistic Accuracy Score ->", accuracy_score(predictions_logistic, test_y)*100)
+""" Certain parts of the code that were used while building the final model 
+have been commented down below, They can be uncommented
+and used as appropriate, they can also be modified and extended as necessary"""
 
+
+# Making WorCloud
+wordcloud = WordCloud(background_color="white", width=3000, height=2000, max_words=300).generate(' '.join(train_df['text'].transpose()))
+# set the figsize
+plt.figure(figsize=[15,10])
+# plot the wordcloud
+plt.imshow(wordcloud, interpolation="bilinear")
+# remove plot axes
+plt.axis("off")
+plt.show()
+
+
+# param_C = [0.01, 0.1, 1, 10, 100]
+# param_grid = {'C': param_C, 'kernel': ['linear', 'poly', 'rbf'], 'gamma': [0.1, 0.01, 0.001]}
+# clf = GridSearchCV(svm.SVC(degree=5,max_iter=10000),cv=5,param_grid=param_grid )
+# clf.fit(train_x_tfidf, train_y)
+# print(clf.best_params_)
+
+
+# # Logistic Classifier
+# logreg = LogisticRegression()
+# logreg.fit(train_x_tfidf, train_y)
+# predictions_logistic = logreg.predict(test_x_tfidf)
+# confusion_matrix_log = confusion_matrix(test_y, predictions_logistic)
+# print("Confusion Matrix for logistic", confusion_matrix_log)
+# print("Logistic Accuracy Score ->", accuracy_score(predictions_logistic, test_y)*100)
+
+
+# train_vectors = tfidf_vectorizer.fit_transform(train_df['text'])
+# test_vectors = tfidf_vectorizer.transform(test_df["text"])
+# SVM.fit(train_vectors,train_df["target"])
+# sample_submission["target"] = Naive.predict(test_vectors)
+# sample_submission.to_csv("submission.csv", index=False)
+
+# # Ridge Classifier
+# clf = linear_model.RidgeClassifier()
+# clf.fit(train_x_tfidf, train_y)
+# predictions_ridge = clf.predict(test_x_tfidf)
+# confusion_matrix_ridge = confusion_matrix(test_y, predictions_ridge)
+# print("confusion matrix for Ridge classifier ", confusion_matrix_ridge)
+# print("Ridge Classifier Accuracy Score ->", accuracy_score(predictions_ridge, test_y)*100)
+
+
+# # Naive Bayes Classifier
+# Naive = naive_bayes.MultinomialNB()
+# Naive.fit(train_x_tfidf, train_y)
+# # predicting the labels on the validation set
+# predictions_NB = Naive.predict(test_x_tfidf)
+# confusion_matrix_nb = confusion_matrix(test_y, predictions_NB)
+# print("Confusion Matrix for Naive Bayes ",confusion_matrix_nb)
+# # Using the accuracy score function to get the accuracy score
+# print("Naive Bayes Accuracy Score ->", accuracy_score(predictions_NB, test_y)*100)
 
